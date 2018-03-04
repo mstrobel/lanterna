7df1ad7ad96dd700fe400b359465e2f54f23bfcb
Mike Strobel <mike.strobel@gmail.com>
2018-03-04 14:11:00 -0500

    Updated test (still WIP)

--- src/test/java/com/googlecode/lanterna/gui2/ScrollViewerTest.java
+++ src/test/java/com/googlecode/lanterna/gui2/ScrollViewerTest.java
@@ -7,6 +7,7 @@ import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.input.KeyStroke;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class ScrollViewerTest extends TestBase {
     public static void main(final String[] args) throws IOException, InterruptedException {
@@ -15,10 +16,58 @@ public class ScrollViewerTest extends TestBase {
 
     @Override
     public void init(final WindowBasedTextGUI textGUI) {
-        final BasicWindow basicWindow = new BasicWindow("ScrollViewer test");
+        final BasicWindow selectorWindow = new BasicWindow("Select a Test");
+
+        selectorWindow.setComponent(
+            new ActionListBox().addItem(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        testCheckerboard(textGUI);
+                    }
+
+                    @Override
+                    public String toString() {
+                        return "Scrollbar policies";
+                    }
+                }
+            ).addItem(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        testFocusedComponentsScrolledIntoView(textGUI);
+                    }
+
+                    @Override
+                    public String toString() {
+                        return "Focused component scrolled into view";
+                    }
+                }
+            ).addItem(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        selectorWindow.close();
+                    }
+
+                    @Override
+                    public String toString() {
+                        return "Exit";
+                    }
+                }
+            )
+        );
+
+        textGUI.addWindow(selectorWindow);
+    }
+
+    private void testCheckerboard(final WindowBasedTextGUI textGUI) {
+        final BasicWindow basicWindow = new BasicWindow("Scrollbar Policy Test");
         final Panel p = new Panel();
 
-        p.setPreferredSize(new TerminalSize(40, 24));
+        basicWindow.setCloseWindowWithEscape(true);
+
+        p.setPreferredSize(new TerminalSize(40, 20));
 
         final ScrollViewer sv = new ScrollViewer();
 
@@ -28,8 +77,31 @@ public class ScrollViewerTest extends TestBase {
         basicWindow.setComponent(p);
 
         textGUI.addWindow(basicWindow);
+        basicWindow.setPosition(TerminalPosition.TOP_LEFT_CORNER);
     }
 
+    private void testFocusedComponentsScrolledIntoView(final WindowBasedTextGUI textGUI) {
+        final BasicWindow basicWindow = new BasicWindow("Bring Into View Test");
+        final Panel p1 = new Panel(new BorderLayout());
+        final Panel p2 = new Panel(new GridLayout(10).setHorizontalSpacing(6).setVerticalSpacing(3));
+        final ScrollViewer sv = new ScrollViewer();
+
+        basicWindow.setCloseWindowWithEscape(true);
+
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                p2.addComponent(new Button("(" + i + ", " + j + ")"));
+            }
+        }
+
+        p1.setPreferredSize(new TerminalSize(20, 10));
+        p1.addComponent(sv, BorderLayout.Location.CENTER);
+        sv.setComponent(p2);
+        basicWindow.setComponent(p1);
+
+        textGUI.addWindow(basicWindow);
+    }
+    
     private final static class DumbComponent extends AbstractInteractableComponent<DumbComponent> {
         @Override
         protected Result handleKeyStroke(KeyStroke keyStroke) {
